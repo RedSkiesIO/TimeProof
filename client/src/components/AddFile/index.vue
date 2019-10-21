fbc4d4a86db49dbb102f6d3f886446b14b01d880
stephenhorsfall <stephen.horsfall@atlascity.io>
2019-10-21 16:57:05 +0100

    remove unused code

--- client/src/components/AddFile/index.vue
+++ client/src/components/AddFile/index.vue
@@ -1,9 +1,12 @@
 <template>
 <div class="q-pa-md">
     <q-uploader
+      flat
+      dark
+      label="Select your File"
+      auto-upload
+      hide-upload-btn
       :factory="factoryFn"
-      multiple
-      style="max-width: 300px"
     />
   </div>
 </template>
@@ -13,7 +16,6 @@ export default {
 
   methods: {
     async factoryFn(files) {
-      // returning a Promise
       const reader = await new FileReader();
       reader.onload = (evt) => {
         const input = Buffer.from(evt.target.result);
@@ -21,13 +23,6 @@ export default {
         console.log('hash:', this.$blake2b(output.length).update(input).digest('hex'));
       };
       reader.readAsArrayBuffer(files[0]);
-
-    //   return new Promise((resolve) => {
-    //     // simulating a delay of 2 seconds
-    //     setTimeout(() => {
-    //       resolve();
-    //     }, 2000);
-    //   });
     },
   },
 };
