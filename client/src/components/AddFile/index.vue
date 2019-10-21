002605c470905954e7aed5d5ae4c12c3365988f2
Stephen <stephenhorsfall5@gmail.com>
2019-10-21 22:31:03 +0100

    add file selected screen

--- client/src/components/AddFile/index.vue
+++ client/src/components/AddFile/index.vue
@@ -1,5 +1,5 @@
 <template>
-<div class="q-pa-md">
+  <div class="q-pa-md">
     <q-uploader
       flat
       bordered
@@ -8,32 +8,76 @@
       hide-upload-btn
       :factory="factoryFn"
     >
-    <template v-slot:list="scope">
-    <div
-    v-if="scope.files < 1"
-    class="q-pa-xl flex flex-center column text-center"
-    style="width:100%; height:100%;">
-    <q-icon name="backup" class="text-grey-4" style="font-size: 100px">
-    </q-icon>
-    <span class="text-h6 text-weight-bold text-grey-6">Drag and Drop your file to sign</span>
-    <span class="text-body1 text-grey-7">
-       or <span class="text-blue" @click="scope.pickFiles()">browse</span> to choose a file</span>
-    </div>
-    </template>
+      <template v-slot:list="scope">
+        <div
+          v-if="scope.files < 1"
+          class="q-pa-xl flex flex-center column text-center"
+          style="width:100%; height:100%;"
+        >
+          <q-icon
+            name="backup"
+            class="text-grey-4"
+            style="font-size: 100px"
+          />
+          <span class="text-h6 text-weight-bold text-grey-6">Drag and Drop your file to sign</span>
+          <span class="text-body1 text-grey-7">
+            or <span
+              class="text-blue"
+              @click="scope.pickFiles()"
+            >browse</span> to choose a file</span>
+        </div>
+        <div
+          v-else
+          class="q-pa-xl flex flex-center column text-center"
+        >
+          <q-icon
+            name="fas fa-file-signature"
+            class="text-grey-4"
+            style="font-size: 100px"
+          />
+          <span class="q-mt-md text-h6 text-primary">
+            {{ file.name }}</span>
+          <span class="text-body1 text-grey-7">
+            type: {{ file.type }}</span>
+          <span class="q-mb-lg text-body1 text-grey-7">
+            size: {{ file.size /1000000 }}mb</span>
+          <q-btn
+            unelevated
+            size="lg"
+            color="primary"
+            label="Sign"
+          />
+          <span
+            class="q-mt-sm text-blue"
+            @click="scope.pickFiles()"
+          >choose a different file</span>
+        </div>
+      </template>
     </q-uploader>
   </div>
 </template>
 <script>
 export default {
-  name: 'addFile',
+  name: 'AddFile',
+  data() {
+    return {
+      file: null,
+    };
+  },
 
   methods: {
     async factoryFn(files) {
+      console.log(files[0]);
+      this.file = {
+        name: files[0].name,
+        type: files[0].type,
+        size: files[0].size,
+      };
       const reader = await new FileReader();
       reader.onload = (evt) => {
         const input = Buffer.from(evt.target.result);
         const output = new Uint8Array(64);
-        console.log('hash:', this.$blake2b(output.length).update(input).digest('hex'));
+        this.file.hash = this.$blake2b(output.length).update(input).digest('hex');
       };
       reader.readAsArrayBuffer(files[0]);
     },
@@ -43,6 +87,7 @@ export default {
 <style lang="scss">
 .q-uploader {
   width: inherit;
+  max-height: inherit;
 }
 .q-uploader__subtitle {
     display: none !important;
