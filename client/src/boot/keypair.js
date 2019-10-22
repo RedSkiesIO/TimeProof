96fe166a8f29ac32a2382ab009d99ee251fa488a
stephenhorsfall <stephen.horsfall@atlascity.io>
2019-10-22 17:50:05 +0100

    change to base32

--- client/src/boot/keypair.js
+++ client/src/boot/keypair.js
@@ -1,33 +1,33 @@
 import Vue from 'vue';
 import tweetnacl from 'tweetnacl';
-import {
-  encodeBase64,
-  decodeBase64,
-} from 'tweetnacl-util';
+import encode from 'base32-encode';
+import decode from 'base32-decode';
+
 
 const keypair = {
   new() {
     const key = tweetnacl.sign.keyPair();
+
     return {
-      publicKey: encodeBase64(key.publicKey),
-      secretKey: encodeBase64(key.secretKey),
+      publicKey: encode(key.publicKey, 'RFC4648'),
+      secretKey: encode(key.secretKey, 'RFC4648'),
     };
   },
 
   newFromSeed(seed) {
     const key = tweetnacl.sign.keyPair.fromSeed(seed);
     return {
-      publicKey: encodeBase64(key.publicKey),
-      secretKey: encodeBase64(key.secretKey),
+      publicKey: encode(key.publicKey, 'RFC4648'),
+      secretKey: encode(key.secretKey, 'RFC4648'),
     };
   },
 
   signMessage(message, secretKey) {
-    return tweetnacl.sign.detached(message, decodeBase64(secretKey));
+    return tweetnacl.sign.detached(message, new Uint8Array(decode(secretKey, 'RFC4648')));
   },
 
   verifyMessage(signedMessage, publicKey) {
-    return tweetnacl.sign.detached.verify(signedMessage, decodeBase64(publicKey));
+    return tweetnacl.sign.detached.verify(signedMessage, new Uint8Array(decode(publicKey, 'RFC4648')));
   },
 
 };
