5b7a773b7f1801f6f25bb99eeea0ca32de35225c
stephenhorsfall <stephen.horsfall@atlascity.io>
2019-10-23 17:13:09 +0100

    remove unused code

--- client/src/components/AddFile/index.vue
+++ client/src/components/AddFile/index.vue
@@ -88,23 +88,6 @@ export default {
       }
       return null;
     },
-
-    // getSize() {
-    //   if (this.file.size) {
-    //     const bytes = this.file.size;
-    //     const decimals = 2;
-    //     if (bytes === 0) return '0 Bytes';
-
-    //     const k = 1024;
-    //     const dm = decimals < 0 ? 0 : decimals;
-    //     const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
-
-    //     const i = Math.floor(Math.log(bytes) / Math.log(k));
-
-    //     return `${parseFloat((bytes / (k ** i)).toFixed(dm))} ${sizes[i]}`;
-    //   }
-    //   return null;
-    // },
   },
 
   methods: {
