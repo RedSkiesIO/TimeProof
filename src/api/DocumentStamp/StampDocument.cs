464a90e3f901e267e2b75423dc3f591146d72671
Alex White <alex.white@atlascity.io>
2019-10-24 17:30:58 +0100

    Add signature verification

--- src/api/DocumentStamp/StampDocument.cs
+++ src/api/DocumentStamp/StampDocument.cs
@@ -10,16 +10,9 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
-
-//using Autofac;
-//using Catalyst.Core.Lib;
-//using Catalyst.Core.Modules.Rpc.Client;
-
-//var containerBuilder = new ContainerBuilder();
-//containerBuilder.RegisterModule<CoreLibProvider>();
-//containerBuilder.RegisterModule<RpcClientModule>();
-//var container = containerBuilder.Build();
-//var rpcClient = container.Resolve<RpcClient>();
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.Crypto.Signers;
+using TheDotNetLeague.MultiFormats.MultiBase;
 
 namespace DocumentStamp
 {
@@ -37,6 +30,28 @@ namespace DocumentStamp
             {
                 var stampDocumentRequest =
                     ModelValidator.ValidateAndConvert<StampDocumentRequest>(await req.ReadAsStringAsync());
+
+                //stampDocumentRequest.PublicKey = "EQWWLY36L3V6OG4UQQKHVIY3ORLFNWZLBNOZBCZWOJX2AT7F3URA".ToLower();
+                //stampDocumentRequest.Hash =
+                //    "4WT6N3R3CQL2QR3X2QA7SN374VN5FQEHYKUXTEXTDA4JWHBZHULSUOGVST6HCAMKELO467I2P24H4MJ3IUTGGGR4YUPUHANTDWMYQSA"
+                //        .ToLower();
+                //stampDocumentRequest.Signature =
+                //    "BGEKWDCR2SLIXKZZGSPRN3ZKKWYGNTGRU2PKO4BLXQJVJVL6NJXFJXLPVVGKBER6FIJTWMKCJ4OXKBLJGEHVT55ODK2OEICR6QF5SDY"
+                //        .ToLower();
+
+
+                var signature = stampDocumentRequest.Signature.FromBase32();
+                var message = stampDocumentRequest.Hash.FromBase32();
+                var publicKey = new Ed25519PublicKeyParameters(stampDocumentRequest.PublicKey.FromBase32(), 0);
+
+                var verifier = new Ed25519Signer();
+                verifier.Init(false, publicKey);
+                verifier.BlockUpdate(message, 0, message.Length);
+                var shouldVerify = verifier.VerifySignature(signature);
+                if (!shouldVerify)
+                {
+                    throw new InvalidDataException("Could not verify signature");
+                }
 
                 var stampDocumentResponse = new StampDocumentResponse
                 {
