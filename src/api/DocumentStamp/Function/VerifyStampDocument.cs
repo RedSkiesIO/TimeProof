32df9c10aeebf3f67aa0e500c5647cccfa16c153
Alex White <alex.white@atlascity.io>
2019-11-18 11:34:03 +0000

    Update VerifyStampDocument

--- src/api/DocumentStamp/Function/VerifyStampDocument.cs
+++ src/api/DocumentStamp/Function/VerifyStampDocument.cs
@@ -13,7 +13,7 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
 using RestSharp;
 
-namespace DocumentStamp.Function
+namespace DocumentStamp.Function    
 {
     public class VerifyStampDocument
     {
