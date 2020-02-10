a38fad76435c5d0e551d8f7f5403056f0ec9defe
stephenhorsfall <stephen.horsfall@atlascity.io>
2020-02-10 16:31:06 +0000

    WIP

--- functions/timestamp/index.js
+++ functions/timestamp/index.js
@@ -4,7 +4,7 @@ const Tx = require('ethereumjs-tx');
 const { jwtDecode, verifyProof } = require('../utils');
 
 
-module.exports = async function timestamp(context, req) {
+module.exports = async function timestamp(context, req, lastNonce) {
   if (req.method !== 'POST') {
     context.log('not verified');
     return {
@@ -32,55 +32,73 @@ module.exports = async function timestamp(context, req) {
 
   const web3js = new Web3(new Web3.providers.HttpProvider(process.env.NODE_API));
   const txData = web3js.utils.asciiToHex(proof);
-  const nonce = await web3js.eth.getTransactionCount(process.env.ACCOUNT_ADDRESS);
-  const rawTransaction = {
-    from: process.env.ACCOUNT_ADDRESS,
-    nonce,
-    gasPrice: web3js.utils.toHex(process.env.GAS_PRICE * 1e9),
-    gasLimit: web3js.utils.toHex(100000),
-    to: process.env.TO_ADDRESS,
-    value: web3js.utils.numberToHex(web3js.utils.toWei('0', 'ether')),
-    data: txData,
-  };
+  const nonce = await web3js.eth.getTransactionCount(process.env.ACCOUNT_ADDRESS, 'pending');
+  let localNonce = nonce;
+  context.log('LocalNonce: ', localNonce);
+  context.log('DatabaseNonce: ', lastNonce[0]);
 
-  const privateKey = Buffer.from(process.env.SECRET_KEY, 'hex');
-  const tx = new Tx.Transaction(rawTransaction, {chain: process.env.CHAIN});
-  tx.sign(privateKey);
-  const serializedTx = tx.serialize();
-  
-  return new Promise((resolve, reject) => {
-    let timestampTx;
-    web3js.eth.sendSignedTransaction(`0x${serializedTx.toString('hex')}`, (error, txHash) => {
-      context.log('error: ', error);
-      if (error) {
-        context.log(error);
-        timestampTx = {
-          status: 400,
-          body: 'Could not verify signature',
+
+  function sendTransaction(accountNonce = nonce) {
+    const rawTransaction = {
+      from: process.env.ACCOUNT_ADDRESS,
+      nonce: accountNonce,
+      gasPrice: web3js.utils.toHex(process.env.GAS_PRICE * 1e9),
+      gasLimit: web3js.utils.toHex(100000),
+      to: process.env.TO_ADDRESS,
+      value: web3js.utils.numberToHex(web3js.utils.toWei('0', 'ether')),
+      data: txData,
+    };
+
+    const privateKey = Buffer.from(process.env.SECRET_KEY, 'hex');
+    const tx = new Tx.Transaction(rawTransaction, { chain: process.env.CHAIN });
+    tx.sign(privateKey);
+    const serializedTx = tx.serialize();
+    return new Promise((resolve, reject) => {
+      let timestampTx;
+      web3js.eth.sendSignedTransaction(`0x${serializedTx.toString('hex')}`, (error, txHash) => {
+        if (error) {
+          context.log(error);
+          if (error.message.includes('same nonce')) {
+            return resolve({
+              body: 'UPDATE_NONCE',
+            });
+          }
+          timestampTx = {
+            status: 400,
+            body: 'Could not verify signature',
+          };
+          return resolve(timestampTx);
+        }
+
+        context.log(txHash);
+        const value = {
+          id: txHash,
+          fileName: req.body.fileName,
+          publicKey: req.body.publicKey,
+          txHash: txHash,
+          timestamp: Date.now(),
+          blockNumber: -1,
+          fileHash: req.body.hash,
+          signature: req.body.signature,
+          nonce: accountNonce,
+          user,
         };
-        return reject(timestampTx);
-      }
-
-      const value = {
-        id: txHash,
-        fileName: req.body.fileName,
-        publicKey: req.body.publicKey,
-        txHash: txHash,
-        timestamp: Date.now(),
-        blockNumber: -1,
-        fileHash: req.body.hash,
-        signature: req.body.signature,
-        user,
-      };
-      context.bindings.timestampDatabase = JSON.stringify(value);
-      context.log(txHash);
-      timestampTx = {
-        body: {
-          success: true,
-          value,
-        },
-      };
-      return resolve(timestampTx);
+        timestampTx = {
+          body: {
+            success: true,
+            value,
+          },
+        };
+        return resolve(timestampTx);
+      });
     });
-  });
+  }
+  let result = await sendTransaction();
+  
+  while (result.body === 'UPDATE_NONCE') {
+
+    localNonce = nonce + 1;
+    result = await sendTransaction(localNonce);
+  }
+  return result;
 };
