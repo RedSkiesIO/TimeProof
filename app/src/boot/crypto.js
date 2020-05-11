ed0cd574a8bae919c5f3a69b0f13d26456fd4eab
Veysel <veyseltosun.vt@gmail.com>
2020-05-11 15:20:49 +0100

    change file type as .txt of keystore

--- app/src/boot/crypto.js
+++ app/src/boot/crypto.js
@@ -65,7 +65,7 @@ const crypto = {
     const dataStr = `data:text/json;charset=utf-8,${encodeURIComponent(JSON.stringify(exportObj))}`;
     const downloadAnchorNode = document.createElement('a');
     downloadAnchorNode.setAttribute('href', dataStr);
-    downloadAnchorNode.setAttribute('download', `${exportName}.json`);
+    downloadAnchorNode.setAttribute('download', `${exportName}.txt`);
     document.body.appendChild(downloadAnchorNode); // required for firefox
     downloadAnchorNode.click();
     downloadAnchorNode.remove();
